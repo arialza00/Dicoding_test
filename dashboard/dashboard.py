3bed76b8bd0e13ddd42f923c957403763373e3c9
arialza00 <72586032+arialza00@users.noreply.github.com>
2025-02-10 12:01:30 +0700

    Update dashboard.py

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -2,7 +2,7 @@ import sys
 import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-import import_ipynb
+import nbformat
 import notebook
 
 # Add the Dicoding folder to the system path
